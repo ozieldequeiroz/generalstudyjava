287f0c1e200e06ab37a797192329c27eb9fe2709
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-03-14 23:05:26 -0300

    update

---
+++ src/javateste/Cliente.java
@@ -0,0 +1,21 @@
+package javateste;
+
+public class Cliente {
+	
+	private Long id;
+	private String nome;
+	
+	public Cliente(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	
+
+}
--- src/javateste/TesteMain.java
+++ src/javateste/TesteMain.java
@@ -1,10 +1,19 @@
 package javateste;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TesteMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		List<Cliente> clientes = new ArrayList();
+		
+		clientes.add(new Cliente(1L,"Joao"));
+		clientes.add(new Cliente(2L,"Maria"));
+		clientes.add(new Cliente(3L,"Jose"));
+		
+		clientes.forEach(c->System.out.printf("ID : %d, Nome : %s%n",c.getId(),c.getNome()));
 	}
 
 }
