9e6b7fbbae88218637e19e669a19e29152298360
Oziel de Queiroz <49464609+ozieldequeiroz@users.noreply.github.com>
2023-10-26 22:36:06 -0300

    update

---
+++ src/javateste/outros/OutrosTeste.java
@@ -0,0 +1,15 @@
+package javateste.outros;
+
+public class OutrosTeste {
+
+	public static void main(String[] args) {
+		int quantidade = 10;
+		int quantidadeOutra = 15;
+		
+		boolean verifica = quantidade <= quantidadeOutra;
+		
+		System.out.println(verifica);
+
+	}
+
+}
